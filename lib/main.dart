94d46fad35662fdf36b243e2f7ac5e0104f1d7f5
trinib <watchtv4ever@gmail.com>
2021-11-30 15:55:48 -0400

    00

--- lib/main.dart
+++ lib/main.dart
@@ -136,6 +136,26 @@ class _MyHomePageState extends State<MyHomePage> {
                         ),
                       ),
                       //// SEARCHBOX
+                      SizedBox(height: 25.0),
+                      Padding(
+                        padding: EdgeInsets.only(left: 15.0, right: 15.0),
+                        child: Material(
+                          elevation: 5.0,
+                          borderRadius: BorderRadius.circular(5.0),
+                          child: TextFormField(
+                              decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  prefixIcon: Icon(Icons.search,
+                                      color: Colors.black87, size: 30.0),
+                                  contentPadding:
+                                      EdgeInsets.only(left: 15.0, top: 15.0),
+                                  hintText: 'Search',
+                                  hintStyle: TextStyle(
+                                      color: Colors.grey,
+                                      fontFamily: 'Quicksand'))),
+                        ),
+                      ),
+                      SizedBox(height: 10.0)
                     ],
                   ),
                 ],
