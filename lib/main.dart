e7f86a65a99c508feab3f8e04eff1963b7fd6a3d
trinib <watchtv4ever@gmail.com>
2021-12-11 19:03:19 -0400

    -

--- lib/main.dart
+++ lib/main.dart
@@ -44,19 +44,17 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
       ///// using 'home' argument sets main/default front page if not using routes to a navigated page
       home: Scaffold(
         bottomNavigationBar: Material(
-          color: null,
           elevation: 30,
           child: TabBar(
             controller: controller,
             labelColor: Colors.black,
             unselectedLabelColor: Colors.amber,
-            indicatorWeight: 5,
-            indicatorColor: Colors.blue,
             indicator: BoxDecoration(
                 borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(10),
-                    topRight: Radius.circular(10)),
-                color: Colors.green),
+                  topLeft: Radius.circular(10),
+                  topRight: Radius.circular(10),
+                ),
+                color: Colors.amber.shade800),
             tabs: const <Widget>[
               Tab(icon: Icon(Icons.event_seat)),
               Tab(icon: Icon(Icons.timer)),
@@ -66,6 +64,7 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
           ),
         ),
         ////+ A 'TABARVIEW' WIDGET IN BODY TO SHOW DIFFERENT PAGES
+        ////* TAB BAR
         body: TabBarView(
           //// need to add controller for 'tabcontroller' method
           controller: controller,
@@ -79,20 +78,4 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
       ),
     );
   }
-}
-
-class ColoredTabBar extends Container implements PreferredSizeWidget {
-  ColoredTabBar(this.color, this.tabBar);
-
-  final Color color;
-  final TabBar tabBar;
-
-  @override
-  Size get preferredSize => tabBar.preferredSize;
-
-  @override
-  Widget build(BuildContext context) => Container(
-        color: color,
-        child: tabBar,
-      );
 }
