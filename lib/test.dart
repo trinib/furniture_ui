841a9d3363aca44f35eff02c28a30fbad86d9798
trinib <watchtv4ever@gmail.com>
2022-07-11 14:43:04 -0400

    -

---
+++ lib/test.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+// ignore: use_key_in_widget_constructors
+class Test extends StatefulWidget {
+  @override
+  _TestState createState() => _TestState();
+}
+
+class _TestState extends State<Test> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
